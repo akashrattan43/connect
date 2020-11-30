4f8c233997003b6490e15053f9aa3243ca0d8878
akash rattan <akashrattan43@Gmail.com>
2020-11-30 15:31:56 -0800

    s

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,6 @@ function App() {
      <Navbar/>
      <Landing />
      <Footer/>
-     <h1>Tommorow work</h1>
-     <h2>Finish the project</h2>
-     <h3>Saturday </h3>
-     <h3>node js connected</h3>
-     <h4>Back end working</h4>
     </div>
   );
 }
